4814ab80920e588c1ef21fda0bb08915c23a1c60
Shruti Vij <67600604+shrutiVij1502@users.noreply.github.com>
2022-07-01 15:22:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ app.get('/will', function (req, res) {
     res.send('{ "response": "Hello World" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": "It works!" }');
+    res.send('{ "response": "It works!, yessss" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
