00ab69872ea1b826e91869cd4990e37703a4f495
Shruti Vij <67600604+shrutiVij1502@users.noreply.github.com>
2022-07-01 13:36:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+    res.send('{ "response": "Hello World , shruti" }');
 });
 app.get('/ready', function (req, res) {
     res.send('{ "response": "It works!" }');
