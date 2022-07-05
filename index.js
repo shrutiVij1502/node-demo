1f5f1b0ea7547fb52e1c8b8ffd43e8470fb2aa67
Shruti Vij <67600604+shrutiVij1502@users.noreply.github.com>
2022-07-05 11:26:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/ready', function (req, res) {
 
 // App
 app.get('/', (req, res) => {
-  res.send('Hello World i am nodejs2');
+  res.send('Hello World i am nodejs3');
 });
 
 app.listen(process.env.PORT || 3000);
